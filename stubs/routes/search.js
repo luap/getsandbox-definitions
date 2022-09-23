8d803a21e457e2dbb5c312f013e9a6e1976df94d
Sandbox Sync <support@getsandbox.com>
2022-09-23 12:50:03 +0000

    Changes

--- stubs/routes/search.js
+++ stubs/routes/search.js
@@ -7,8 +7,17 @@
  *
  */
 exports.postSearch = function(req, res) {
-	res.status(401);
+	res.status(200);
 
 	// set response body and send
-	res.json({});
+	res.json({
+  "id": "67917474-4036-fbc9-0f17-36a2119f0ec9",
+  "email": "200@tmw.test",
+  "preferences": [
+    "Post"
+  ],
+  "source": "Salesforce",
+  "lastModifiedBy": "exercitation Lorem",
+  "lastModifiedDate": "1990-06-14T03:10:40.678Z"
+});
 };
